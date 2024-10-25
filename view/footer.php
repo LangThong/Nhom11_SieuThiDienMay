0a7ca484036a2ef33cf6d8d50e4f46b8db67f58a
LangThong <thanhthuantanan@gmail.com>
2024-10-25 22:50:18 +0700

    first commit

---
+++ view/footer.php
@@ -0,0 +1,25 @@
+	
+	<footer>
+		<div class="top-footer container-fluid">
+			<div class="container">
+				<div class="mini-footer col-lg-4 col-md-4 col-sm-4 col-xs-4">
+					<p class="title">Về Chúng Tôi</p>
+					<p>Chúng tôi là một trong những siêu thị điện máy lớn nhất ở South Dakota và khu vực Midwest. Dòng sản phẩm của chúng tôi đa dạng từ thiết bị điện tử tiêu dùng như tivi đến các thiết bị gia dụng như tủ lạnh, máy giặt và các thiết bị thông minh khác.</p>
+					</div>
+				<div class="mini-footer col-lg-4 col-md-4 col-sm-4 col-xs-4">
+					<p class="title">Dịch Vụ Khách Hàng</p>
+					<p><a href="">TRANG CHỦ</a></p>
+	    			<p><a href="">CỬA HÀNG</a></p>
+	    			<p><a href="">TIN TỨC</a></p>
+	    			<p><a href="">HỖ TRỢ</a></p>
+	    			<p><a href="">LIÊN HỆ</a></p>
+				</div>
+				<div class="mini-footer col-lg-4 col-md-4 col-sm-4 col-xs-4">
+					<p class="title">Thông Tin Liên Hệ</p>
+					<p>Địa chỉ:Số 26 Đường 26 Phước Long A TP.Thủ Đức<br><br>SĐT:0855003466</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
