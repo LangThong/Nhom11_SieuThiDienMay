0c57225a3eaf9c363c256feef60de2858e1fa558
Giang <trinhgianhsoi@gmail.com>
2024-10-25 23:36:41 +0700

    up layout login va register

--- Admin/register.php
+++ Admin/register.php
@@ -4,13 +4,13 @@
 	$user=$pass=$email=NULL;
 	if (isset($_POST["ok"])) {
 		if (empty($_POST["user"])) {
-			$tb["username"]="Vui lòng nhập username";
+			$tb["username"]="Vui lòng nhập tên tài khoản";
 		}
 		else{
 			$user=$_POST["user"];
 		}
 		if (empty($_POST["pass"])) {
-			$tb["password"]="Vui lòng nhập password";
+			$tb["password"]="Vui lòng nhập mật khẩu";
 		}
 		else{
 			$pass=$_POST["pass"];
@@ -42,9 +42,12 @@
 	<link rel="stylesheet" type="text/css" href="../css/styles.css">
 </head>
 <body style="background: #F7F7F7">
-	<div class="login" style="width:600px;margin:auto;position:relative;">
+	<div class="login" style="width:600px;margin:auto;position:relative; top: 40px;box-shadow: 5px 4px 8px rgba(0,0,0, .08);">
 			<center><i class="fa fa-user-plus" aria-hidden="true" style="font-size:100px;"></i></center>
 			<form class="form-horizontal" action="register.php" method="post">
+			    <div class="text-center">
+					<h1>Đăng Ký</h1>
+				</div>
 				<div class="form-group">
 				    <label for="inputPassword3" class="col-sm-3 control-label">&nbsp</label>
 				    <div class="col-sm-8">
@@ -52,27 +55,27 @@
 				    </div>
 				</div>
 				<div class="form-group">
-				    <label class="col-sm-3 control-label">USER NAME</label>
+				    <label class="col-sm-3 control-label">Tên tài khoản</label>
 				    <div class="col-sm-8">
-				    	<input type="text" name="user" class="form-control" placeholder="User name">
+				    	<input type="text" name="user" class="form-control" placeholder="Nhập tên tài khoản">
 				    </div>
 				</div>
 				<div class="form-group">
-				    <label for="inputPassword3" class="col-sm-3 control-label">PASSWORD</label>
+				    <label for="inputPassword3" class="col-sm-3 control-label">Mật Khẩu</label>
 				    <div class="col-sm-8">
-				    	<input type="password" class="form-control" name="pass" placeholder="Password">
+				    	<input type="password" class="form-control" name="pass" placeholder="Nhập mật khẩu">
 				    </div>
 				</div>
 				<div class="form-group">
-				    <label class="col-sm-3 control-label">EMAIL</label>
+				    <label class="col-sm-3 control-label">Email</label>
 				    <div class="col-sm-8">
 				    	<input type="email" name="email" class="form-control" placeholder="Email">
 				    </div>
 				</div>
 				<div class="form-group">
-				    <div class="col-sm-offset-3 col-sm-8">
-				    	<button type="submit" name="ok" class="btn btn-default">Đăng Ký</button>
-				    	<a href="login.php" class="btn btn-default">Đăng Nhập</a>
+				    <div class="col-sm-offset-4  col-sm-8">
+				    	<button type="submit" name="ok" class="btn btn-primary">Đăng Ký</button>
+				    	<a href="login.php" class="btn btn-primary">Đăng Nhập</a>
 				    </div>
 				</div>
 			</form>
